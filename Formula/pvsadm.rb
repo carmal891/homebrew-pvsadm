66d67284f7d22fb83e2ced20e930e738e5d79037
bumperbot <54744891+carmal891@users.noreply.github.com>
2024-09-22 15:48:05 +0000

    pvsadm 0.1.17

--- Formula/pvsadm.rb
+++ Formula/pvsadm.rb
@@ -1,7 +1,7 @@
 class Pvsadm < Formula
   desc "Tool for managing the IBM Power Systems Virtual Servers"
   homepage "https://github.com/ppc64le-cloud/pvsadm"
-  version "0.1.11"
+  version "0.1.17"
   license "Apache-2.0"
   on_macos do
     on_intel do
@@ -17,8 +17,8 @@ class Pvsadm < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.1.11/pvsadm-linux-amd64.tar.gz"
-      sha256 "b25e9453bdd75a11af3303783c250c93f05b3472cfe49e795bab200e5db04e03"
+      url "https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.1.17/pvsadm-linux-amd64.tar.gz"
+      sha256 "0ece0d5674770af26fab167f3c63e8dc49c8947bef15cd745a4e45b4c255932c"
     end
   end
 
