8c94ef80638871ac260829f10f07986b72472de0
carmal891 <carmal911@gmail.com>
2024-09-23 11:38:17 +0530

    syntactically fixes rb files

--- Formula/pvsadm_backup.rb
+++ Formula/pvsadm_backup.rb
@@ -1,7 +1,7 @@
 class PvsadmBackup < Formula
   desc "Tool for managing the IBM Power Systems Virtual Servers"
   homepage "https://github.com/ppc64le-cloud/pvsadm"
-  version "0.1.17"
+  version "0.1.11"
   license "Apache-2.0"
   on_macos do
     on_intel do
@@ -17,7 +17,7 @@ class PvsadmBackup < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.1.17/pvsadm-linux-amd64.tar.gz"
+      url "https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.1.11/pvsadm-linux-amd64.tar.gz"
       sha256 "0ece0d5674770af26fab167f3c63e8dc49c8947bef15cd745a4e45b4c255932c"
     end
   end
