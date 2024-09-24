95a260f615d37a69070528e9e87f2c292ad6995b
carmal891 <carmal911@gmail.com>
2024-09-24 19:14:05 +0530

    adds dummy checksum, version and url for paremt formula to suppress lints

--- Formula/pvsadm.rb
+++ Formula/pvsadm.rb
@@ -2,8 +2,9 @@ class Pvsadm < Formula
   desc "Tool for managing the IBM Power Systems Virtual Servers"
   homepage "https://github.com/ppc64le-cloud/pvsadm"
   license "Apache-2.0"
-  url "https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.0.1.tar.gz"
-  sha256 "0ece0d5674770af26fab167f3c63e8dc49c8947bef15cd745a4e45b4c255932c"
+  version "1.0.0"
+  url "https://github.com/ppc64le-cloud/pvsadm"
+  sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
 
 
   if OS.mac?
