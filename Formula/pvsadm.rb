598742de4d65ab275dc7ce36df72158d7b3fba27
carmal891 <carmal911@gmail.com>
2024-09-24 20:05:30 +0530

    pvsadm

--- Formula/pvsadm.rb
+++ Formula/pvsadm.rb
@@ -3,22 +3,22 @@ class Pvsadm < Formula
   homepage "https://github.com/ppc64le-cloud/pvsadm"
   license "Apache-2.0"
 
-  def install
-    if OS.mac?
-      if Hardware::CPU.intel?
-        require_relative "pvsadm_darwin_amd64"
-        @formula = PvsadmDarwinAmd64.new
-      elsif Hardware::CPU.arm?
-        require_relative "pvsadm_darwin_arm64"
-        @formula = PvsadmDarwinArm64.new
-      end
-    elsif OS.linux?
-      if Hardware::CPU.intel?
-        require_relative "pvsadm_linux_amd64"
-        @formula = PvsadmLinuxAmd64.new
-      end
+  if OS.mac?
+    if Hardware::CPU.intel?
+      require_relative "pvsadm_darwin_amd64"
+      PvsadmDarwinAmd64.new
+    elsif Hardware::CPU.arm?
+      require_relative "pvsadm_darwin_arm64"
+      PvsadmDarwinArm64.new
     end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      require_relative "pvsadm_linux_amd64"
+      PvsadmLinuxAmd64.new
+    end
+  end
 
+  def install
     raise "Unsupported platform" unless defined? @formula
 
     @formula.install
